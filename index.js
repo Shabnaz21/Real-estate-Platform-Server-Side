cb8fbb84926b9955bc22ac92b774d18d151cf37d
Shabnaz21 <shabnaz608@gmail.com>
2023-11-28 08:01:03 +0600

    user delete

--- index.js
+++ index.js
@@ -59,6 +59,12 @@ async function run() {
             res.send(result);
         });
 
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await UserCollection.deleteOne(query);
+            res.send(result);
+        })
 
         // reviews collection
         app.get('/reviews', async (req, res) => {
